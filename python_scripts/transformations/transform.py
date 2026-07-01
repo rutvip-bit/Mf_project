e7b65b678daac65f4c4ee3a7e47c850a04518c49
Rutvi <rutvi.p@devstree.in>
2026-07-01 10:33:40 +0530

    commit

--- python_scripts/transformations/transform.py
+++ python_scripts/transformations/transform.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 engine = create_engine(
     "postgresql+psycopg2://postgres:postgres123@localhost:5432/tr_project"
@@ -19,18 +19,23 @@ def load_silver():
     # INVESTOR MASTER
     # =========================
     investor_df = safe_read("""
-        SELECT * FROM bronze.investor_master
+        SELECT *
+        FROM bronze.investor_master
         WHERE flag = 0
     """)
 
     if not investor_df.empty:
+
         investor_df = investor_df.drop(columns=["flag"], errors="ignore")
+
+        with engine.begin() as conn:
+            conn.execute(text("TRUNCATE TABLE silver.investor_master"))
 
         investor_df.to_sql(
             "investor_master",
             engine,
             schema="silver",
-            if_exists="replace",
+            if_exists="append",
             index=False,
             chunksize=5000,
             method="multi"
@@ -40,39 +45,49 @@ def load_silver():
     # TRANSACTION
     # =========================
     transaction_df = safe_read("""
-        SELECT * FROM bronze.transaction
+        SELECT *
+        FROM bronze."transaction"
         WHERE flag = 0
     """)
 
     if not transaction_df.empty:
+
         transaction_df = transaction_df.drop(columns=["flag"], errors="ignore")
+
+        with engine.begin() as conn:
+            conn.execute(text('TRUNCATE TABLE silver."transaction"'))
 
         transaction_df.to_sql(
             "transaction",
             engine,
             schema="silver",
-            if_exists="replace",
+            if_exists="append",
             index=False,
             chunksize=5000,
             method="multi"
         )
 
     # =========================
-    # SIP (OPTIONAL SAFE)
+    # SIP INFO
     # =========================
     sip_df = safe_read("""
-        SELECT * FROM bronze.sip_info
+        SELECT *
+        FROM bronze.sip_info
         WHERE flag = 0
     """)
 
     if not sip_df.empty:
+
         sip_df = sip_df.drop(columns=["flag"], errors="ignore")
+
+        with engine.begin() as conn:
+            conn.execute(text("TRUNCATE TABLE silver.sip_info"))
 
         sip_df.to_sql(
             "sip_info",
             engine,
             schema="silver",
-            if_exists="replace",
+            if_exists="append",
             index=False,
             chunksize=5000,
             method="multi"
